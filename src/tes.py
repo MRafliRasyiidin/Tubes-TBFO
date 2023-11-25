b5f9b3d90df6a3e645c03d045aa5a4d181741235
b33rk <brkmanics@gmail.com>
2023-11-25 14:13:05 +0700

    feat: add attribute tokenizer in main

--- src/tes.py
+++ src/tes.py
@@ -46,10 +46,18 @@ def readTag():
     global currentChar
     global tag
     tag = ''
-    while currentChar != ">":
+    while currentChar != blank or currentChar != ">":
         tag += currentChar
         currentChar = f.read(1)
-    return tag
+    if currentChar == blank:
+        ignoreBlank()
+        if currentChar == ">":
+            return tag, True
+        else :
+            return tag, False
+    else:
+        return tag, True
+        
 
 def createPDA(states, input_symbol, stack, start_state, start_stack, final_state, transition_function):
     global currentChar
@@ -98,11 +106,66 @@ createPDA(states, input_symbol, stack, start_state, start_stack, final_state, tr
 start_state = start_state[0]
 start_stack = start_stack[0]
 final_state = final_state[0]
+stack = []
+list = []
 f.close()
+
+def attr():
+    attr = ""
+    while currentChar != "\"" or currentChar != blank:
+        x = f.read(1)
+        attr += x
+        
+    if attr != "type=\"" or attr != "method=\"":
+        if currentChar == blank:
+            ignoreBlank()
+        if currentChar == "=":
+            attr += currentChar
+            x = f.read(1)
+            ignoreBlank()
+            if currentChar == "\"":
+                attr += currentChar
+                while currentChar != "\"" and currentChar != "":
+                    x = f.read(1)
+                if currentChar == "":
+                    g = False
+                attr += x
+            else :
+                g = False
+        else:
+            g = False
+    else:
+        if currentChar == blank:
+            ignoreBlank()
+        if currentChar == "=":
+            attr += currentChar
+            x = f.read(1)
+            ignoreBlank()
+            if currentChar == "\"":
+                attr += currentChar
+                while currentChar != "\"" and currentChar != "":
+                    x = f.read(1)
+                    attr += x
+                if currentChar == "":
+                    g = False
+            else :
+                g = False
+        else:
+            g = False
 
 f = open("testing.html", "r")
 currentChar = f.read(1)
 for line in f:
     if currentChar == "<":
-        x = readTag()
-    
+        x, cur = readTag()
+        stack.append(x)
+        ignoreBlank()
+        if not cur:
+            attr()
+        else:
+            if currentChar != ">":
+                g = False
+            else :
+                stack.append(">")
+
+# <html id=""
